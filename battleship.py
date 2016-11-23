197620821d9f75a9d3799ccc58bca066f60d7740
Char Aznable <charazn37@gmail.com>
2016-11-24 00:06:37 +0800

    Improve game logic

--- battleship.py
+++ battleship.py
@@ -28,37 +28,39 @@ def set_ship_position(board):
 for x in range(3):
     set_ship_position(board)
 
-print_board(board)
+# print_board(board)
 
 ships_sunk = 0
 
 for turn in range(5):
-    global ships_sunk
-
     print "Turn", turn + 1
 
-    guess_row = int(raw_input("Guess Row: "))
-    guess_col = int(raw_input("Guess Col: "))
+    guess_row = int(raw_input("Guess Row (Enter a number 0 - 4): "))
+    guess_col = int(raw_input("Guess Col (Enter a number 0 - 4): "))
 
-    if board[guess_row][guess_col] == "S":
+    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+        print "Oops, that's not even in the ocean."
+    elif board[guess_row][guess_col] == "S":
+        global ships_sunk
         print "Congratulations! You sunk one of my battleship!"
         board[guess_row][guess_col] = "X"
         ships_sunk += 1
-        print ships_sunk
+        # print ships_sunk
         if ships_sunk == 3:
             print "Congratulations! You sunk all of my battleships!"
             print_board(board)
             break
-        print_board(board)
+        # print_board(board)
     else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print "Oops, that's not even in the ocean."
-        elif (board[guess_row][guess_col] == "M"):
+        if (board[guess_row][guess_col] == "M"):
             print "You guessed that one already."
         else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "M"
 
-        if turn == 3:
-            print "Game Over"
+    if turn == 4:
+        print "Game Over"
         print_board(board)
+        print "X - Destroyed ship"
+        print "M - Missed"
+        print "S - Ship position"
