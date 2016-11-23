88c8172452e03889015d0881474b21b895e187c0
Char Aznable <charazn37@gmail.com>
2016-11-23 23:53:20 +0800

    Add 3 ships to game. Game ends when sink all 3 in 5 turns

--- battleship.py
+++ battleship.py
@@ -13,30 +13,47 @@ print "Let's play Battleship!"
 
 def random_row(board):
     return randint(0, len(board) - 1)
-
 def random_col(board):
     return randint(0, len(board[0]) - 1)
+def set_ship_position(board):
+    ship_row = random_row(board)
+    ship_col = random_col(board)
 
-ship_row = random_row(board)
-ship_col = random_col(board)
-board[ship_row][ship_col] = "S"
+    if board[ship_row][ship_col] == "S":
+        print "Position already selected. Randomly selecting another position..."
+        set_ship_position(board)
+    else:
+        board[ship_row][ship_col] = "S"
 
-# print_board(board)
+for x in range(3):
+    set_ship_position(board)
 
-for turn in range(4):
+print_board(board)
+
+ships_sunk = 0
+
+for turn in range(5):
+    global ships_sunk
+
     print "Turn", turn + 1
 
     guess_row = int(raw_input("Guess Row: "))
     guess_col = int(raw_input("Guess Col: "))
 
-    if guess_row == ship_row and guess_col == ship_col:
-        print "Congratulations! You sunk my battleship!"
+    if board[guess_row][guess_col] == "S":
+        print "Congratulations! You sunk one of my battleship!"
+        board[guess_row][guess_col] = "X"
+        ships_sunk += 1
+        print ships_sunk
+        if ships_sunk == 3:
+            print "Congratulations! You sunk all of my battleships!"
+            print_board(board)
+            break
         print_board(board)
-        break
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
             print "Oops, that's not even in the ocean."
-        elif(board[guess_row][guess_col] == "M"):
+        elif (board[guess_row][guess_col] == "M"):
             print "You guessed that one already."
         else:
             print "You missed my battleship!"
@@ -44,4 +61,4 @@ for turn in range(4):
 
         if turn == 3:
             print "Game Over"
-        # print_board(board)
+        print_board(board)
